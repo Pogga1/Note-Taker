36435dd2103bc89eb6213f6608c1fc3e25f548cc
Pogga1 <Andreweysoldt@gmail.com>
2022-10-18 14:04:52 -0700

    fixed routing issues

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const dataBase = require('./Develop/db/db.json');
+const dataBase = require('./db/db.json');
 const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
@@ -9,15 +9,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
 app.get('/api/notes', (req, res) => {
     res.json(dataBase.slice(1));
 });
+
 app.post('/api/notes', (req, res) => {
     const newNote = genNote(req.body, dataBase);
     res.json(newNote);
@@ -37,7 +41,7 @@ const genNote = (body, notesArray) => {
     notesArray[0]++ ;
     notesArray.push(newNote);
 
-    fs.writeFileSync(path.join(__dirname, '/Develop/db/db.json'),
+    fs.writeFileSync(path.join(__dirname, '/db/db.json'),
         JSON.stringify(notesArray, null, 2 )
     );
     return newNote;
@@ -48,7 +52,7 @@ for (let i = 0; i < notesArray.length; i++) {
     let note = notesArray[i];
     if (note.id == id) {
         notesArray.splice(i, 1);
-        fs.writeFileSync(path.join(__dirname, '/Develop/db/db.json'),
+        fs.writeFileSync(path.join(__dirname, '/db/db.json'),
             JSON.stringify(notesArray, null, 2)
         );
         break;
